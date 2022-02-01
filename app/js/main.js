80445a47ff3aca01904b5c6dc2460fafc4a8e673
Dobrodei <Dobrodei.98@gmail.com>
2022-02-01 19:41:55 +0300

    add section scheme and faq

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
 $(function () {
-  
+  const headers = document.querySelectorAll(".faq__head");
+
+  headers.forEach(function (item) {
+    item.addEventListener('click', function () {
+      if (item.classList.contains('faq__head--active')) {
+        item.classList.remove('faq__head--active');
+        this.nextElementSibling.classList.remove('faq__text--active');
+      } else {
+        item.classList.add('faq__head--active');
+        this.nextElementSibling.classList.add('faq__text--active');
+      }
+    });
+  })
 })
