2f78f7b523cffc7b298699a122ffa42eb48ff14c
Dobrodei <Dobrodei.98@gmail.com>
2022-02-03 02:11:05 +0300

    add section feedback, faq,footer, fix scheme

--- app/js/main.js
+++ app/js/main.js
@@ -12,4 +12,40 @@ $(function () {
       }
     });
   })
+
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  $('.other-job__tab').on('click', function (e) {
+    e.preventDefault();
+    $('.other-job__tab').removeClass('other-job__tab--active')
+    $(this).addClass('other-job__tab--active');
+
+    $('.other-job__item').removeClass('other-job__item--active')
+    $($(this).attr('href')).addClass('other-job__item--active')
+  });
+
+  const workSlider = new Swiper('.work-process__slider', {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  $('.other-job__tab').on('click', function (e) {
+    e.preventDefault();
+    $('.other-job__tab').removeClass('other-job__tab--active')
+    $(this).addClass('other-job__tab--active');
+
+    $('.other-job__item').removeClass('other-job__item--active')
+    $($(this).attr('href')).addClass('other-job__item--active')
+  });
 })
--- gulpfile.js
+++ gulpfile.js
@@ -64,6 +64,7 @@ function htmlInclude() {
 function scripts() {
   return src([
       'node_modules/jquery/dist/jquery.js',
+      'node_modules/swiper/swiper-bundle.js',
       'app/js/main.js'
     ])
     .pipe(concat('main.min.js'))
