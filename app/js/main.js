7405b8e497309578ade43d233bb715846226367c
Dobrodei <Dobrodei.98@gmail.com>
2022-02-04 03:11:01 +0300

    add section portfolio,reviews, and adaptive page

--- app/js/main.js
+++ app/js/main.js
@@ -31,15 +31,6 @@ $(function () {
     $($(this).attr('href')).addClass('other-job__item--active')
   });
 
-  const workSlider = new Swiper('.work-process__slider', {
-    slidesPerView: 3,
-    spaceBetween: 10,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
-
   $('.other-job__tab').on('click', function (e) {
     e.preventDefault();
     $('.other-job__tab').removeClass('other-job__tab--active')
@@ -48,4 +39,122 @@ $(function () {
     $('.other-job__item').removeClass('other-job__item--active')
     $($(this).attr('href')).addClass('other-job__item--active')
   });
+
+  if ($(".portfolio__swiper").length) {
+    var swiperBig = new Swiper(".portfolio__swiper", {
+      slidesPerView: 1,
+      spaceBetween: 5,
+      updateOnWindowResize: !0,
+      navigation: {
+        nextEl: "[data-work-example] .swiper-button-next",
+        prevEl: "[data-work-example] .swiper-button-prev",
+      },
+      pagination: {
+        el: ".work-example__swiper-pagination",
+        clickable: !0,
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 19,
+          updateOnWindowResize: !0,
+        },
+      },
+    });
+    swiperBig.on("imagesReady", function () {
+        swiperBigFunc(), swiperBig.update();
+      }),
+      swiperBig.on("beforeResize", function () {
+        swiperBigFunc();
+      }),
+      swiperBig.on("orientationchange", function () {
+        swiperBigFunc();
+      });
+  }
+
+  function swiperBigFunc() {
+    var e = $(".portfolio__item.swiper-slide").eq(0).width();
+    $(".portfolio__inner").css("width", e);
+  }
+  if (
+    ($(".switch-slide__before").addClass("switch-slide__before--active"),
+      $(".switch-slide__before").on("click", (e) => {
+        $(".portfolio__item.swiper-slide").eq(1) && (swiperBig.slideTo(0, 800, !0), $(".switch-slide__after").removeClass("switch-slide__after--active"), $(".switch-slide__before").addClass("switch-slide__before--active"));
+      }),
+      $(".switch-slide__after").on("click", (e) => {
+        $(".portfolio__item.swiper-slide").eq(0) && (swiperBig.slideTo(1, 800, !0), $(".switch-slide__before").removeClass("switch-slide__before--active"), $(".switch-slide__after").addClass("switch-slide__after--active"));
+      }),
+      $(".portfolio__subswiper-container").length)
+  ) {
+    var swiperMini = new Swiper(".portfolio__subswiper-container", {
+      slidesPerView: 7,
+      spaceBetween: 7,
+      centeredSlides: !0,
+      loop: !0,
+      slideToClickedSlide: !0,
+      navigation: {
+        nextEl: "[data-work-example-mini] .swiper-button-next",
+        prevEl: "[data-work-example-mini] .swiper-button-prev",
+      },
+      pagination: {
+        el: ".work-example-mini__swiper-pagination",
+        clickable: !0,
+      },
+      breakpoints: {
+        600: {
+          spaceBetween: 10,
+        },
+      },
+    });
+    swiperMini.on("slideChange", function () {
+      var e = swiperMini.realIndex;
+      console.log(e);
+      var a = $("[data-swiper-slide-index=" + e + "]").data("img-mini"),
+        t = $("[data-swiper-slide-index=" + e + "]").data("title-mini"),
+        i = $("[data-swiper-slide-index=" + e + "]").data("day-mini"),
+        r = $("[data-swiper-slide-index=" + e + "]").data("total-mini"),
+        n = $(".portfolio__swiper .swiper-slide");
+      swiperBig.slideTo(0, 800, !0),
+        $(".portfolio__swiper .swiper-slide").removeAttr("href"),
+        $(".portfolio__swiper .swiper-slide img").removeAttr("src"),
+        $(".portfolio__subtitle").text(t),
+        $("[data-total] span").text(r),
+        $("[data-day] span").text(i);
+      for (let e = 0; e < a.length; e++) $(n[e]).attr("href", a[e]), $(n[e]).find("img").attr("src", a[e]);
+    });
+  }
+
+  const workSlider = new Swiper('.work-process__slider', {
+    slidesPerView: 3,
+    spaceBetween: 12,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  const reviewSlider = new Swiper('.reviews__slider-container', {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  // var tileSlider = document.querySelectorAll('.slider');
+  // tileSlider.forEach(function (n) {
+  //   var mySwiper = new Swiper(n.querySelector('.swiper'), {
+  //     slidesPerView: 'auto',
+  //     spaceBetween: 24,
+  //     navigation: {
+  //       nextEl: n.querySelector('.slider-bullets__btn--next'),
+  //       prevEl: n.querySelector('.slider-bullets__btn--prev'),
+  //     },
+  //     scrollbar: {
+  //       el: '.swiper-scrollbar',
+  //       draggable: true,
+  //     },
+  //   });
+  // });
 })
