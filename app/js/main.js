a5cb16e5b69120428e960d8142b3da23db012af2
Dobrodei <Dobrodei.98@gmail.com>
2022-02-05 00:02:39 +0300

    adaptive faq section, feedback, services

--- app/js/main.js
+++ app/js/main.js
@@ -228,4 +228,32 @@ $(function () {
       }
     });
   });
+  // $('input').change(function () {
+  //   var label = $(this).parent().find('span');
+  //   if (typeof (this.files) != 'undefined') { // fucking IE      
+  //     if (this.files.length == 0) {
+  //       label.removeClass('withFile').text(label.data('default'));
+  //     } else {
+  //       var file = this.files[0];
+  //       var name = file.name;
+  //       var size = (file.size / 1048576).toFixed(3); //size in mb 
+  //       label.addClass('withFile').text(name + ' (' + size + 'mb)');
+  //     }
+  //   } else {
+  //     var name = this.value.split("\\");
+  //     label.addClass('withFile').text(name[name.length - 1]);
+  //   }
+  //   return false;
+  // });
+  let fileInput = document.getElementById("file-upload-input");
+  let fileSelect = document.getElementsByClassName("form__label")[0];
+  fileSelect.onclick = function () {
+    fileInput.click();
+  }
+  fileInput.onchange = function () {
+    let filename = fileInput.files[0].name;
+    let selectName = document.getElementsByClassName("file-select-button")[0];
+    selectName.innerText = filename;
+  }
+
 })
