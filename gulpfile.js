846c58469630158edfe0adf0c22c3b72afbcb82c
Dobrodei <Dobrodei.98@gmail.com>
2022-02-09 14:54:55 +0300

    fix fonts

--- gulpfile.js
+++ gulpfile.js
@@ -66,6 +66,7 @@ function scripts() {
       'node_modules/jquery/dist/jquery.js',
       'node_modules/swiper/swiper-bundle.js',
       'node_modules/inputmask/dist/inputmask.js',
+      'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
       'app/js/main.js'
     ])
     .pipe(concat('main.min.js'))
